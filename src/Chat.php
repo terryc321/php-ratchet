13a56123acd7261fd1537f49b609a35d4fb26f82
terry <terry.cadd@gmail.com>
2025-09-13 14:10:02 +0100

    made src/chat.php bin/chat-server gitignore files

---
+++ src/Chat.php
@@ -0,0 +1,21 @@
+
+<?php
+namespace MyApp;
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    public function onOpen(ConnectionInterface $conn) {
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+    }
+}
+
+
